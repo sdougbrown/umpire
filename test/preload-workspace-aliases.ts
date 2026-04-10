1ba0858a178fdc064089da95f0c5c1339107cb73
Doug Brown <doug@github.douggo.com>
2026-04-10 14:55:18 -0400

    ⚙️  Bun Test Config Adjustments

--- test/preload-workspace-aliases.ts
+++ test/preload-workspace-aliases.ts
@@ -1,51 +1,27 @@
 import { mock } from 'bun:test'
+import { createRequire } from 'node:module'
+
+const require = createRequire(import.meta.url)
 
 if (process.env.BUN_DISABLE_WORKSPACE_MOCKS !== 'true') {
-  const core = await import('../packages/core/src/index.js')
-  mock.module('@umpire/core', () => core)
-
-  const store = await import('../packages/store/src/index.js')
-  mock.module('@umpire/store', () => store)
-
-  const reads = await import('../packages/reads/src/index.js')
-  mock.module('@umpire/reads', () => reads)
-
-  const react = await import('../packages/react/src/index.js')
-  mock.module('@umpire/react', () => react)
-
-  const signals = await import('../packages/signals/src/index.js')
-  mock.module('@umpire/signals', () => signals)
-
-  const testing = await import('../packages/testing/src/index.js')
-  mock.module('@umpire/testing', () => testing)
-
-  const zod = await import('../packages/zod/src/index.js')
-  mock.module('@umpire/zod', () => zod)
-
-  const json = await import('../packages/json/src/index.js')
-  mock.module('@umpire/json', () => json)
-
-  const devtools = await import('../packages/devtools/src/index.js')
-  mock.module('@umpire/devtools', () => devtools)
-
-  const devtoolsSlim = await import('../packages/devtools/src/slim.js')
-  mock.module('@umpire/devtools/slim', () => devtoolsSlim)
-
-  const devtoolsReact = await import('../packages/devtools/entrypoints/react.js')
-  mock.module('@umpire/devtools/react', () => devtoolsReact)
-
-  const redux = await import('../packages/redux/src/index.js')
-  mock.module('@umpire/redux', () => redux)
-
-  const pinia = await import('../packages/pinia/src/index.js')
-  mock.module('@umpire/pinia', () => pinia)
-
-  const tanstackStore = await import('../packages/tanstack-store/src/index.js')
-  mock.module('@umpire/tanstack-store', () => tanstackStore)
-
-  const vuex = await import('../packages/vuex/src/index.js')
-  mock.module('@umpire/vuex', () => vuex)
-
-  const zustand = await import('../packages/zustand/src/index.js')
-  mock.module('@umpire/zustand', () => zustand)
+  mock.module('@umpire/core', () => require('../packages/core/src/index.js'))
+  mock.module('@umpire/store', () => require('../packages/store/src/index.js'))
+  mock.module('@umpire/reads', () => require('../packages/reads/src/index.js'))
+  mock.module('@umpire/react', () => require('../packages/react/src/index.js'))
+  mock.module('@umpire/signals', () => require('../packages/signals/src/index.js'))
+  mock.module('@umpire/testing', () => require('../packages/testing/src/index.js'))
+  mock.module('@umpire/zod', () => require('../packages/zod/src/index.js'))
+  mock.module('@umpire/json', () => require('../packages/json/src/index.js'))
+  mock.module('@umpire/devtools', () => require('../packages/devtools/src/index.js'))
+  mock.module('@umpire/devtools/slim', () => require('../packages/devtools/src/slim.js'))
+  mock.module('@umpire/devtools/react', () =>
+    require('../packages/devtools/entrypoints/react.js'),
+  )
+  mock.module('@umpire/redux', () => require('../packages/redux/src/index.js'))
+  mock.module('@umpire/pinia', () => require('../packages/pinia/src/index.js'))
+  mock.module('@umpire/tanstack-store', () =>
+    require('../packages/tanstack-store/src/index.js'),
+  )
+  mock.module('@umpire/vuex', () => require('../packages/vuex/src/index.js'))
+  mock.module('@umpire/zustand', () => require('../packages/zustand/src/index.js'))
 }
