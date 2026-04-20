ac91c7e83e6b8ec20af25a817c97ccd6001d2c38
Doug Brown <doug@github.douggo.com>
2026-04-19 23:42:22 -0400

    🪠 Fix solid mock setup
    Love these instances where there are differences between local and CI.
    This is one of those cases! Working around bun's module resolution also
    led to multiple instances of the same package because they pointed at
    different locations. Cool.

--- packages/solid/test-setup.ts
+++ packages/solid/test-setup.ts
@@ -4,4 +4,5 @@ import { createRequire } from 'node:module'
 const require = createRequire(import.meta.url)
 
 mock.module('solid-js', () => require('solid-js/dist/solid.cjs'))
+mock.module('solid-js/dist/solid.js', () => require('solid-js/dist/solid.cjs'))
 mock.module('solid-js/store', () => require('solid-js/store/dist/store.cjs'))
