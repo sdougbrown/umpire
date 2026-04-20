da71fe6b2185d52a26d745d8d8f669f15c7b1d55
Doug Brown <doug@github.douggo.com>
2026-04-19 22:12:02 -0400

    ✅ Remove deprecated React act warnings in devtools tests

--- packages/devtools/happydom.ts
+++ packages/devtools/happydom.ts
@@ -1,3 +1,5 @@
 import { GlobalRegistrator } from '@happy-dom/global-registrator'
 
 GlobalRegistrator.register()
+
+;(globalThis as { IS_REACT_ACT_ENVIRONMENT?: boolean }).IS_REACT_ACT_ENVIRONMENT = true
